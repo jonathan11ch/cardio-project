f0084112f54ba7e7e2130b8858945b381dfe93bf
jonathan11ch <jonathan11ch@hotmail.com>
2017-01-13 19:14:45 -0500

    Add files via upload

--- src/cardio_wigets.py
+++ src/cardio_wigets.py
@@ -2,6 +2,7 @@ import sys
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 import res.resources_rc
+import core_lib as cl
 
 
 # widget classes for sensor status
@@ -18,9 +19,9 @@ class LedWidget(QLabel):
         self.set_pixmap(0)
 
         self.show()
-    
+
     def get_status(self):
-        return self.value        
+        return self.value
 
     def set_pixmap(self, value):
 
@@ -31,11 +32,11 @@ class LedWidget(QLabel):
             self.value = value
             self.pixmap.load(':img/orange_bulb.png')
         elif value == 2:
-            self.value = value 
+            self.value = value
             self.pixmap.load(':img/green_bulb.png')
 
         self.pixmap = self.pixmap.scaled(20,20, Qt.KeepAspectRatio)
-        
+
         if self.pixmap.isNull():
             print("Null image...")
         else:
@@ -63,12 +64,30 @@ class SensorWidget(QWidget):
     	#self.hbox.addStretch(0)
         self.hbox.addWidget(self.label)
         self.hbox.addWidget(self.led)
-        
-        self.setLayout(self.hbox) 
 
+        self.setLayout(self.hbox)
+
+''' SensorStatusWidget:
+    This class inherits the QWidget class
+    the object displayes the widget for the sensor monitoring
+    the monitor displayes four SensorWidget object that show the status of each sensor
+    in addition this class provides methods to control the and consult the status of each SensorWidget
+'''
 class SensorStatusWidget(QWidget):
+
+    """
+        __init__ method override
+    """
     def __init__(self):
         super(SensorStatusWidget, self).__init__()
+        self.init()
+
+    ''' init:
+        This function initialize all the object
+        creates the basic variables
+        set the layout and the widget style
+    '''
+    def init(self):
         self.title = QLabel()
         #--------setup---------
         self.title.setText("SENSOR STATUS")
@@ -80,28 +99,31 @@ class SensorStatusWidget(QWidget):
         self.Lfont.setBold(True)
         self.title.setFont(self.Lfont)
 
-
+        #sensor widget variables
         self.laser = SensorWidget("Laser: ")
         self.imu = SensorWidget("IMU: ")
         self.nao = SensorWidget("Nao: ")
         self.ecg = SensorWidget("Ecg: ")
 
-        #self.laser.setAlignment(Qt.AlignCenter)
-
+        #layout object
         self.vbox = QVBoxLayout()
-        
+
         self.vbox.addWidget(self.title)
         self.vbox.addWidget(self.laser)
         self.vbox.addWidget(self.imu)
         self.vbox.addWidget(self.ecg)
         self.vbox.addWidget(self.nao)
-        #self.vbox.addStretch(0)
 
         self.setGeometry(0, 0, 200, 200)
         self.setLayout(self.vbox)
 
+    ''' get_sensor_status:
+        returns the status of an specific sensor s
+        In parameters = String s (sensor)
+        Out parameters = Intege 0,1,2 (status)
+    '''
     def get_sensor_status(self, s):
-        
+
         if s == "laser":
             return self.laser.led.get_status()
 
@@ -115,8 +137,13 @@ class SensorStatusWidget(QWidget):
             return self.ecg.led.get_status()
 
         else:
-            return -1 
+            return -1
 
+    ''' set_sensor_state:
+        set the status of an specific sensor widget
+        In parameters = String s (sensor), Integer 0,1,2 (status)
+        Out parameters = Integer -1 if there is an exception
+    '''
     def set_sensor_state(self, sensor, state):
 
         if sensor == "laser":
@@ -132,9 +159,9 @@ class SensorStatusWidget(QWidget):
             return self.ecg.led.set_pixmap(state)
 
         else:
-            return -1    
-         
-# widget classes for therapy parameters 
+            return -1
+
+# widget classes for therapy parameters
 class ParameterWidget(QWidget):
 
     def __init__(self, t,u):
@@ -158,7 +185,7 @@ class ParameterWidget(QWidget):
         self.value_label.setText(self.value)
         self.value_label.setFixedWidth(50)
         self.value_label.show()
-        #unit label 
+        #unit label
         self.unit = QLabel()
         self.unit.setText(u)
         self.unit.setFont(self.Lfont)
@@ -173,16 +200,39 @@ class ParameterWidget(QWidget):
         #self.hbox.addStretch(0)
         self.hbox.addWidget(self.unit)
 
-        self.setLayout(self.hbox) 
+        self.setLayout(self.hbox)
         self.show()
 
     def set_value(self, l):
-        self.value = l
-        self.value_label.setText(str(self.value))
+        if  not l == "-":
+            self.value = l
+            self.value_label.setText(str(self.value))
+        else:
+            print("leaving parameter unchanged")
 
+    def get_value(self):
+        return self.value_label.text()
+
+'''ParameterMonitor
+    this class inherits the object QWidget
+    The widget displayes all the parameters of the therapy
+    provides methods to set the values either individualy (set_parameter_value)
+    or as a hole list (set_parameter_list)
+'''
 class ParameterMonitor(QWidget):
+    '''
+        __init__ method override
+    '''
     def __init__(self):
         super(ParameterMonitor, self).__init__()
+        self.init()
+
+    ''' init:
+        This function initialize all the object
+        creates the basic variables
+        set the layout and the widget style
+    '''
+    def init(self):
         #set title
         self.title = QLabel()
         self.title.setText("THERAPY PARAMETERS")
@@ -203,7 +253,7 @@ class ParameterMonitor(QWidget):
         self.borg_scale = ParameterWidget("Borg Scale: ", "range")
         #set values
         #self.cadence.set_value(1)
-        #set layout 
+        #set layout
         self.vbox = QVBoxLayout()
         self.vbox.addWidget(self.cadence)
         self.vbox.addWidget(self.step_length)
@@ -213,7 +263,7 @@ class ParameterMonitor(QWidget):
         self.vbox.addWidget(self.borg_scale)
         self.vbox.addStretch(0)
         self.vbox.setSpacing(0)
-        
+
 
         self.hbox = QVBoxLayout()
         self.hbox.addWidget(self.title)
@@ -221,6 +271,46 @@ class ParameterMonitor(QWidget):
         self.setLayout(self.hbox)
         self.setGeometry(0, 0, 400, 100)
         self.show()
+
+    '''set_parameter_value:
+        set the value to an specific parameter
+        In parameters: String value, Strin param
+        Out parameters: Integer -1 if something wrong
+    '''
+    def set_parameter_value(self, value, param):
+        if param == "cadence":
+            self.cadence.set_value(value)
+        elif param == "step_length":
+            self.step_length.set_value(value)
+        elif param  == "speed":
+            self.speed.set_value(value)
+        elif param == "inclination":
+            self.inclination.set_value(value)
+        elif param == "heart_beats":
+            self.heart_beats.set_value(value)
+        elif param == "borg_scale":
+            self.borg_scale.set_value(value)
+        else:
+            return -1
+
+    ''' set_parameter_list:
+        set the values to all the parameters of the monitor
+        In parameters: List[] l -- All parameter values
+        Out parameters: return msg if something wrong with the format
+    '''
+    def set_parameter_list(self, l):
+        print len(l)
+        if len(l) == 6:
+            self.cadence.set_value(l[0])
+            self.step_length.set_value(l[1])
+            self.speed.set_value(l[2])
+            self.inclination.set_value(l[3])
+            self.heart_beats.set_value(l[4])
+            self.borg_scale.set_value(l[5])
+        else:
+            print("wrong data string ")
+            return
+
 
 # widget  classes for Control panel
 class ControlPanelWidget(QWidget):
@@ -241,7 +331,7 @@ class ControlPanelWidget(QWidget):
         self.stop_button = QPushButton("Stop Therapy")
         self.borg_button = QPushButton("Borg Scale")
         self.save_button = QPushButton("Save data")
-        
+
         #horizontal box layout
         self.hbox = QHBoxLayout()
         self.hbox.addWidget(self.run_button)
@@ -264,39 +354,85 @@ class NetworkLabel(QWidget):
         self.label = QLabel()
         self.ip = QLabel()
         self.hbox = QHBoxLayout()
-        self.label.setText("Ip Adrress: ")
+        self.label.setText("Ip Address: ")
         self.ip.setText("127.0.0.1")
         self.hbox.addWidget(self.label)
         self.hbox.addWidget(self.ip)
         self.setLayout(self.hbox)
 
-class NetworkMonitor(QWidget):
-    def __init__(self):
-        super(NetworkMonitor, self).__init__()
-        #self title
-        self.title = QLabel()
-        self.title.setText("NETWORK MONITOR")
-        self.title.setAlignment(Qt.AlignCenter)
-        self.palette = QPalette()
-        self.palette.setColor(QPalette.Foreground,Qt.red)
-        self.title.setPalette(self.palette)
-        self.Lfont = QFont()
-        self.Lfont.setBold(True)
-        self.title.setFont(self.Lfont)
+    def __init__(self, b):
+    	super(NetworkLabel, self).__init__()
+    	print(b)
+    	self.label = QLabel()
+        self.ip = QLabel()
+        self.hbox = QHBoxLayout()
+        self.label.setText("Ip Address: ")
+        self.ip.setText("127.0.0.1")
 
-        self.localhost = NetworkLabel()
-        self.vbox = QVBoxLayout()
-        self.vbox.addWidget(self.title)
-        self.vbox.addWidget(self.localhost)
-        self.vbox.addStretch(0)
-        self.setLayout(self.vbox)
+    	self.hbox.addWidget(self.label)
+        self.hbox.addWidget(self.ip)
+
+    	if  b:
+    		self.button = QCommandLinkButton()
+        	self.hbox.addWidget(self.button)
+        	#self.q = NetworkRequestWidget()
+        self.setLayout(self.hbox)
+
+class NetworkMonitor(QWidget):
+
+	def __init__(self):
+		super(NetworkMonitor, self).__init__()
+		#self title
+		self.title = QLabel()
+		self.title.setText("NETWORK MONITOR")
+		self.title.setAlignment(Qt.AlignCenter)
+		self.palette = QPalette()
+		self.palette.setColor(QPalette.Foreground,Qt.red)
+		self.title.setPalette(self.palette)
+		self.Lfont = QFont()
+		self.Lfont.setBold(True)
+		self.title.setFont(self.Lfont)
+
+		self.localhost = NetworkLabel(False)
+
+		self.nao_network = NetworkLabel(True)
+		self.nao_network.label.setText("Nao Address: ")
+		self.nao_network.ip.setText("----")
+		self.nao_network.button.clicked.connect(self.showDialog)
+
+		self.vbox = QVBoxLayout()
+		self.vbox.addWidget(self.title)
+		self.vbox.addWidget(self.localhost)
+		self.vbox.addWidget(self.nao_network)
+		self.vbox.addStretch(0)
+
+		self.setLayout(self.vbox)
+
+	def get_robot_ip(self):
+		return self.nao_network.ip.text()
+
+
+	def setCallback(self, func):
+		self.func = func
+
+	def Callback(self):
+		self.func()
+
+
+	def showDialog(self):
+		ip, ok = QInputDialog.getText(self, 'Input Dialog','Enter the Nao IP:')
+		if ok:
+			print (ip)
+			self.nao_network.ip.setText(ip)
+			self.Callback()
+
 
 #Main window class
 class CentralWindow(QWidget):
     def __init__(self):
         super(CentralWindow, self).__init__()
         self.init_grid()
-        
+
     #grid layout
     def init_grid(self):
         self.grid = QGridLayout()
@@ -310,7 +446,7 @@ class CentralWindow(QWidget):
         self.grid.addWidget(self.parameter_monitor,1,2,2,1)
         self.grid.addWidget(self.control_panel,3,1,1,2)
         self.setLayout(self.grid)
-    #box layout 
+    #box layout
     def init_box(self):
         self.parameter_monitor = ParameterMonitor()
         self.sensor_status = SensorStatusWidget()
@@ -319,8 +455,8 @@ class CentralWindow(QWidget):
         self.hbox = QHBoxLayout()
         self.hbox.addWidget(self.sensor_status)
         self.hbox.addWidget(self.parameter_monitor)
-        
-        
+
+
         self.vbox = QVBoxLayout()
         self.vbox.addLayout(self.hbox)
         self.vbox.addWidget(self.control_panel)
--- src/core_lib.py
+++ src/core_lib.py
@@ -3,18 +3,42 @@ import os
 import socket
 #sensor state checker functions 
 #each function returns the state of the syensor (0,1,2)
+'''
+This function check the connection beetween the pc and the Nao robot 
+The functions performs a ping to test the connection
+if a response is recieved then returns 2 (the connection is up)
+otherwise returns 0 (connection down)
+
+In parameters: String "NAO Ip" 
+Out Parameters: Integer 0 or 2 depending of the state of the connection
+'''
+
 def nao_checker(nao_ip):
 	#ping to nao
-	hostname = nao_ip
-	response = os.system("ping -c 1 " + hostname)
-	if response == 0:
-		print(hostname + " is up")	
-		return 2
+	if not (nao_ip == "----"):
+		hostname = nao_ip
+		response = os.system("ping -c 1 " + hostname)
+		if response == 0:
+			print(hostname + " is up")	
+			return 2
+		else:
+			print("Nao unreachable, please verify that the robot is on and connected to the same network")  	
+			return 0
 	else:
-		print("Nao unreachable, please verify that the robot is on and connected to the same network")  	
-		return 0
+		print("Wrong ip format")
+		
+
+'''
+This functions checks the pc's internet connection connection and the ip Address
+Creates a socket in order to stablish a simple connection with a normal server (google.com) 
+After the connection ask for the IP address
+
+In parameters: void
+Out Parameters: String "Ip address" if successfull and Integer -1 if no internet conection
+'''
 
 def localhost_checker():
+
 	hostname = "google.com"
 	response = os.system("ping -c 1 " + hostname)
 	if response == 0:
@@ -28,22 +52,25 @@ def localhost_checker():
 	else:
 		print("Not connected to the network")
 		return -1
-		
+
+'''
+This function checks on the system (ubuntu) the connected devices
+particulary search the hokuyo and imu device
+
+In Parameters: void
+Out Parameters: tuple [Laser_state, imu_state]
+'''	
 def check_devices():
-	devices = ["/dev/" + x for x in os.popen("ls /dev/ | egrep -i 'ttyUSB|ttyS[01]$'").read().strip().split('\n')]
+	#check dev folder for serial (usb) connected sensors
+	devices = ["/dev/" + x for x in os.popen("ls /dev/ | egrep -i 'hokuyo|imu'").read().strip().split('\n')]
 	print(devices)
 	laser_state = 0
 	imu_state = 0 
 	for dev in devices:
-		if dev == "/dev/ttyS1":
+		if dev == "/dev/hokuyo":
 			laser_state = 2
 		elif dev == "/dev/imu":
 			imu_state = 2
 	return [laser_state, imu_state]
-	#localhost_checker()	
-#def laser_checker():
-	#check port on dev 
-	#check permision 
-	#return 
 	
 
